fb5e427097daad31971cbd79300939f813b695a1
andres.traks@gmail.com <andres.traks@gmail.com@1fc5ae1f-e7ff-89c7-c687-95c3f8911453>
2011-12-06 12:19:55 +0000

    Create static BluetoothManager class.

--- trunk/PhotoSharing/src/ee/ut/cs/mobile/ShareActivity.java
+++ trunk/PhotoSharing/src/ee/ut/cs/mobile/ShareActivity.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.Set;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -26,7 +23,6 @@ public class ShareActivity extends Activity {
 	private ArrayList<String> items = new ArrayList<String>();
 	private ArrayAdapter<String> itemAdapter;
 
-	private BluetoothAdapter mBluetoothAdapter;
 	private BroadcastReceiver mReceiver;
 
 	@Override
@@ -34,8 +30,7 @@ public class ShareActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.share);
 
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
+		if (!BluetoothManager.isBluetoothSupported()) {
 			Toast.makeText(this, "Bluetooth not supported!", Toast.LENGTH_LONG)
 					.show();
 			finish();
@@ -54,41 +49,32 @@ public class ShareActivity extends Activity {
 			}
 		});
 
-		if (mBluetoothAdapter.isEnabled()) {
+		BluetoothManager.enable(this, REQUEST_ENABLE_BT);
+		if (BluetoothManager.isEnabled()) {
 			searchBluetoothDevices();
-		} else {
-			Intent enableBtIntent = new Intent(
-					BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}
+
+		final Activity activity = this;
 		Button discoverable = (Button) findViewById(R.id.discoverable);
 		discoverable.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View arg0) {
-				ensureDiscoverable();
+				BluetoothManager.ensureDiscoverable(activity);
 			}
 		});
 	}
 
 	private void searchBluetoothDevices() {
-		if (mBluetoothAdapter == null) {
-			return;
+		Set<BluetoothDevice> pairedDevices = BluetoothManager.getBondedDevices();
+
+		// Loop through paired devices
+		for (BluetoothDevice device : pairedDevices) {
+			// Add the name and address to an array adapter to show in a
+			// ListView
+			itemAdapter.add(device.getName() + "\n" + device.getAddress());
 		}
 
-		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
-				.getBondedDevices();
-		// If there are paired devices
-		if (pairedDevices.size() > 0) {
-			// Loop through paired devices
-			for (BluetoothDevice device : pairedDevices) {
-				// Add the name and address to an array adapter to show in a
-				// ListView
-				itemAdapter.add(device.getName() + "\n" + device.getAddress());
-			}
-		}
-
-		// Create a BroadcastReceiver for ACTION_FOUND
-		mReceiver = new BroadcastReceiver() {
+		BluetoothManager.startDeviceSearch(this, new BroadcastReceiver() {
 			public void onReceive(Context context, Intent intent) {
 				String action = intent.getAction();
 				// When discovery finds a device
@@ -106,13 +92,7 @@ public class ShareActivity extends Activity {
 					}
 				}
 			}
-		};
-		// Register the BroadcastReceiver
-		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-		registerReceiver(mReceiver, filter); // Don't forget to unregister
-												// during onDestroy
-
-		mBluetoothAdapter.startDiscovery();
+		});
 	}
 
 	@Override
@@ -127,17 +107,9 @@ public class ShareActivity extends Activity {
 
 	@Override
 	protected void onDestroy() {
-		unregisterReceiver(mReceiver);
-		super.onDestroy();
-	}
-
-	private void ensureDiscoverable() {
-		if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-			Intent discoverableIntent = new Intent(
-					BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-			discoverableIntent.putExtra(
-					BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-			startActivity(discoverableIntent);
+		if (mReceiver != null) {
+			BluetoothManager.stopDeviceSearch(this, mReceiver);
 		}
+		super.onDestroy();
 	}
 }
